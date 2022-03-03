78acecee1d0d5c569227235c316da93b08b2f696
Joeran <joeran@bosma.co>
2022-03-03 16:35:35 +0100

    SOL links /input to read-only folder

--- projects/Task019_ADAM/submission/scripts/inference.sh
+++ projects/Task019_ADAM/submission/scripts/inference.sh
@@ -2,8 +2,8 @@
 . /activate
 
 # bring adam file into internal structure
-cp /input/pre/struct_aligned.nii.gz ${det_data}/${ENVTASK}/raw_splitted/imagesTs/case_0000.nii.gz
-cp /input/pre/TOF.nii.gz ${det_data}/${ENVTASK}/raw_splitted/imagesTs/case_0001.nii.gz
+cp /output/pre/struct_aligned.nii.gz ${det_data}/${ENVTASK}/raw_splitted/imagesTs/case_0000.nii.gz
+cp /output/pre/TOF.nii.gz ${det_data}/${ENVTASK}/raw_splitted/imagesTs/case_0001.nii.gz
 
 nndet_predict ${ENVTASK} ${ENVMODEL} -f -1
 cp ${det_models}/${ENVTASK}/${ENVMODEL}/${ENVFOLD}/test_predictions/* ${det_results}
